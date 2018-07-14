97fc6f49a322882f1a33a78f411217fbda12bfb0
Gabriel Feo <gabriel@gabrielfeo.com>
2018-07-13 21:35:08 -0300

    Created initial activities classes and layout structure

---
+++ app/src/main/java/com/gabrielfeo/backintheday/MovieDetailActivity.java
@@ -0,0 +1,20 @@
+package com.gabrielfeo.backintheday;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_movie_detail);
+		setupToolbar();
+	}
+
+	private void setupToolbar() {
+		setSupportActionBar(findViewById(R.id.moviedetail_t_toolbar));
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+}
