5d938f0c4b0f960566e3a78c889ce70c583cfc42
Gabriel Feo <gabriel@gabrielfeo.com>
2018-07-16 19:56:06 -0300

    Implemented the RecyclerView in the Activity and its ViewModel

--- app/src/main/java/com/gabrielfeo/backintheday/MoviesListActivity.java
+++ app/src/main/java/com/gabrielfeo/backintheday/MoviesListActivity.java
@@ -1,13 +1,25 @@
 package com.gabrielfeo.backintheday;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MoviesListActivity extends AppCompatActivity {
+
+	private MoviesListViewModel viewModel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_movies_list);
+		viewModel = ViewModelProviders.of(this).get(MoviesListViewModel.class);
+		setupRecyclerView();
+	}
+
+	private void setupRecyclerView() {
+		RecyclerView recyclerView = findViewById(R.id.list_rv_movies_list);
+		recyclerView.setHasFixedSize(true);
+		recyclerView.setAdapter(new MovieAdapter(viewModel.getMovies()));
 	}
 }
