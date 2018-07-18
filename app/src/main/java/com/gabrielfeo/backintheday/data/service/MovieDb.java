62d5807992c0eeaf0b4205b60935f0e3852fa208
Gabriel Feo <gabriel@gabrielfeo.com>
2018-07-18 19:26:06 -0300

    Configured Retrofit to append an external API key
    Set Retrofit to use an OkHttp Client that appends an API key to all
    URLs. The API key should be passed to Gradle as an external property
    with the key EXT_MOVIEDB_API_KEY, and the MovieDb class will read it
    from the BuildConfig class.

--- app/src/main/java/com/gabrielfeo/backintheday/data/service/MovieDb.java
+++ app/src/main/java/com/gabrielfeo/backintheday/data/service/MovieDb.java
@@ -1,11 +1,18 @@
 package com.gabrielfeo.backintheday.data.service;
 
+import com.gabrielfeo.backintheday.BuildConfig;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public final class MovieDb {
 
 	private static final String BASE_URL = "https://api.themoviedb.org/3/";
+	private static final String MOVIEDB_API_KEY = BuildConfig.MOVIEDB_API_KEY;
 	private static Retrofit retrofit;
 
 	public static MovieService getMovieService() {
@@ -20,8 +27,25 @@ public final class MovieDb {
 	private static void buildRetrofit() {
 		retrofit = new Retrofit.Builder()
 				.baseUrl(BASE_URL)
+				.client(getNewClient())
 				.addConverterFactory(GsonConverterFactory.create())
 				.build();
+	}
+
+	private static OkHttpClient getNewClient() {
+		return new OkHttpClient.Builder().addNetworkInterceptor(getApiKeyInterceptor()).build();
+	}
+
+	private static Interceptor getApiKeyInterceptor() {
+		return chain -> {
+			Request oldRequest = chain.request();
+			HttpUrl originalUrl = oldRequest.url();
+			HttpUrl newUrl = originalUrl.newBuilder()
+			                            .addQueryParameter("api_key", MOVIEDB_API_KEY)
+			                            .build();
+			Request newRequest = oldRequest.newBuilder().url(newUrl).build();
+			return chain.proceed(newRequest);
+		};
 	}
 
 }
