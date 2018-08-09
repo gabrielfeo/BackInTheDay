650d2df898994c643af2863c74cf9abf60fe38a4
Gabriel Feo <gabriel@gabrielfeo.com>
2018-08-09 02:32:00 -0300

    Created a class for getting a poster URL
    The MoviePosterUrl class returns a full poster image URL from a path
    that is obtained from the regular API (the image is served
    independently of the API).
    
    This was preferred over implementing another OkHttp Interceptor because
    the poster image–fetching won't pass through Retrofit at all, being
    downloaded directly with Picasso instead. The Interceptor
    implementation considered is in commit 506d0eb8.

---
+++ app/src/main/java/com/gabrielfeo/backintheday/data/url/MoviePosterUrl.java
@@ -0,0 +1,16 @@
+package com.gabrielfeo.backintheday.data.url;
+
+import android.net.Uri;
+
+public class MoviePosterUrl {
+
+	private static final String POSTER_BASE_URL = "https://image.tmdb.org/t/p/";
+	private static final String POSTER_SIZE = "w342";
+
+	private MoviePosterUrl() {}
+
+	public static Uri getFor(String path) {
+		return Uri.parse(POSTER_BASE_URL + POSTER_SIZE + path);
+	}
+
+}
