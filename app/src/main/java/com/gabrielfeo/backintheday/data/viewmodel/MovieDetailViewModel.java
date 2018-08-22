bd79b45c64523105db73359e9310a1557abc81b8
Gabriel Feo <gabriel@gabrielfeo.com>
2018-08-22 14:16:14 -0300

    Implemented Movie Detail Activity and ViewModel
    The Movie Detail Activity and ViewModel were written following the
    previous pattern of LiveData-observing views and the new
    ApiResponseHandler.

---
+++ app/src/main/java/com/gabrielfeo/backintheday/data/viewmodel/MovieDetailViewModel.java
@@ -0,0 +1,48 @@
+package com.gabrielfeo.backintheday.data.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+
+import com.gabrielfeo.backintheday.R;
+import com.gabrielfeo.backintheday.data.callback.ApiResponseHandler;
+import com.gabrielfeo.backintheday.data.callback.ErrorCallback;
+import com.gabrielfeo.backintheday.data.callback.SuccessCallback;
+import com.gabrielfeo.backintheday.data.model.Movie;
+import com.gabrielfeo.backintheday.data.service.MovieDb;
+
+public class MovieDetailViewModel extends AndroidViewModel {
+
+	private static final String TAG = MovieDetailViewModel.class.getSimpleName();
+	private int movieId;
+	private MutableLiveData<Movie> movie = new MutableLiveData<>();
+
+	public MovieDetailViewModel(@NonNull Application application) {
+		super(application);
+	}
+
+	public LiveData<Movie> getMovie(ErrorCallback errorCallback) {
+		if (movie.getValue() == null) {
+			refreshMovieDetails(errorCallback);
+		}
+		return movie;
+	}
+
+	private void refreshMovieDetails(ErrorCallback errorCallback) {
+		SuccessCallback<Movie> successCallback = movie::setValue;
+		String errorMessage = getApplication().getString(R.string.moviedetail_error_getting_details);
+		MovieDb.getMovieService()
+		       .getMovieWithId(movieId)
+		       .enqueue(new ApiResponseHandler<Movie>(successCallback, errorCallback, errorMessage));
+	}
+
+	public int getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+}
--- app/src/main/java/com/gabrielfeo/backintheday/ui/activity/MovieDetailActivity.java
+++ app/src/main/java/com/gabrielfeo/backintheday/ui/activity/MovieDetailActivity.java
@@ -1,22 +1,76 @@
 package com.gabrielfeo.backintheday.ui.activity;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gabrielfeo.backintheday.R;
+import com.gabrielfeo.backintheday.data.callback.ErrorCallback;
+import com.gabrielfeo.backintheday.data.model.Movie;
+import com.gabrielfeo.backintheday.data.viewmodel.MovieDetailViewModel;
+import com.squareup.picasso.Picasso;
 
 public class MovieDetailActivity extends AppCompatActivity {
+
+	private static final String EXTRA_MOVIE_KEY = "movie";
+	private MovieDetailViewModel viewModel;
+
+	private CoordinatorLayout rootView;
+	private ImageView posterView;
+	private TextView titleView;
+
+	private final ErrorCallback snackbarErrorCallback = message -> {
+		Snackbar.make(rootView, "", Snackbar.LENGTH_LONG)
+		        .setAction("retry", view -> observeMovie())
+		        .setText(message)
+		        .show();
+	};
+
+	public static Intent getNewIntent(Context context, int movieId) {
+		Intent intent = new Intent(context, MovieDetailActivity.class);
+		intent.putExtra(EXTRA_MOVIE_KEY, movieId);
+		return intent;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		viewModel = ViewModelProviders.of(this).get(MovieDetailViewModel.class);
 		setContentView(R.layout.activity_movie_detail);
+		findViews();
 		setupToolbar();
+		setMovieIdFromIntent();
+		observeMovie();
+	}
+
+	private void findViews() {
+		rootView = findViewById(R.id.moviedetail_cl_root);
+		titleView = findViewById(R.id.moviedetail_tv_title);
+		posterView = findViewById(R.id.moviedetail_iv_poster);
 	}
 
 	private void setupToolbar() {
 		setSupportActionBar(findViewById(R.id.moviedetail_t_toolbar));
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	private void setMovieIdFromIntent() {
+		viewModel.setMovieId(getIntent().getIntExtra(EXTRA_MOVIE_KEY, -1));
+	}
+
+	private void observeMovie() {
+		viewModel.getMovie(snackbarErrorCallback).observe(this, this::fillViewsWithMovie);
+	}
+
+	private void fillViewsWithMovie(Movie movie) {
+		titleView.setText(movie.getTitle());
+		Picasso.get().load(movie.getPosterUrl()).into(posterView);
 	}
 
 }
