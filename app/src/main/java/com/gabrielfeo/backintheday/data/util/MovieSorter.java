fb8935c78287d16b6fddcfdc1c2e5acb50acdbe2
Gabriel Feo <gabriel@gabrielfeo.com>
2019-01-12 13:21:38 -0200

    Changed 'if' wrapping style, formatted MovieSorter

--- app/src/main/java/com/gabrielfeo/backintheday/data/util/MovieSorter.java
+++ app/src/main/java/com/gabrielfeo/backintheday/data/util/MovieSorter.java
@@ -25,17 +25,17 @@ public final class MovieSorter {
 
     private void sortByPopularityDescending() {
         movies.sort((movie1, movie2) -> {
-            if (movie1.getPopularity() < movie2.getPopularity()) {
-                return 1;
-            } else if (movie1.getPopularity() > movie2.getPopularity()) { return -1; } else return 0;
+            if (movie1.getPopularity() < movie2.getPopularity()) return 1;
+            else if (movie1.getPopularity() > movie2.getPopularity()) return -1;
+            else return 0;
         });
     }
 
     private void sortByRatingDescending() {
         movies.sort((movie1, movie2) -> {
-            if (movie1.getVoteAverage() < movie2.getVoteAverage()) {
-                return 1;
-            } else if (movie1.getVoteAverage() > movie2.getVoteAverage()) { return -1; } else return 0;
+            if (movie1.getVoteAverage() < movie2.getVoteAverage()) return 1;
+            else if (movie1.getVoteAverage() > movie2.getVoteAverage()) return -1;
+            else return 0;
         });
     }
 
