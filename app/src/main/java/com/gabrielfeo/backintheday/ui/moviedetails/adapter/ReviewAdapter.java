65f6a084783d0095763ef0306a6a3f45e31915bb
Gabriel Feo <gabriel@gabrielfeo.com>
2019-01-17 20:50:50 -0200

    Created ReviewAdapter
    Other changes:
     - Renamed a view ID in the review item layout, for consistency.

---
+++ app/src/main/java/com/gabrielfeo/backintheday/ui/moviedetails/adapter/ReviewAdapter.java
@@ -0,0 +1,76 @@
+package com.gabrielfeo.backintheday.ui.moviedetails.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gabrielfeo.backintheday.R;
+import com.gabrielfeo.backintheday.data.callback.ErrorCallback;
+import com.gabrielfeo.backintheday.model.Review;
+
+import java.util.List;
+
+import static android.support.design.widget.Snackbar.LENGTH_SHORT;
+
+public final class ReviewAdapter extends RecyclerView.Adapter<ReviewViewHolder> {
+
+    private List<Review> reviews;
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    @NonNull
+    @Override
+    public ReviewViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View reviewView = inflater.inflate(R.layout.item_movie_review, viewGroup, false);
+        return new ReviewViewHolder(reviewView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReviewViewHolder viewHolder, int position) {
+        setAuthorFrom(reviews.get(position), viewHolder);
+        setContentFrom(reviews.get(position), viewHolder);
+        setReadMoreButtonAction(reviews.get(position), viewHolder);
+    }
+
+    private void setAuthorFrom(Review review, ReviewViewHolder viewHolder) {
+        String author = review.getAuthor();
+        viewHolder.authorView.setText(author);
+    }
+
+    private void setContentFrom(Review review, ReviewViewHolder viewHolder) {
+        String content = review.getContent();
+        viewHolder.contentView.setText(content);
+    }
+
+    private void setReadMoreButtonAction(Review review, ReviewViewHolder viewHolder) {
+        ErrorCallback errorCallback = () -> Snackbar.make(viewHolder.itemView,
+                                                          R.string.moviedetail_reviews_read_more_error, LENGTH_SHORT);
+        viewHolder.readMoreButton.setOnClickListener(
+                view -> sendOpenReviewIntent(view.getContext(), review.getUrl(), errorCallback));
+    }
+
+    private void sendOpenReviewIntent(Context context, String reviewUrl, ErrorCallback errorCallback) {
+        Uri parsedUri = Uri.parse(reviewUrl);
+        if (parsedUri != null) {
+            Intent viewIntent = new Intent(Intent.ACTION_VIEW, parsedUri);
+            context.startActivity(viewIntent);
+        } else {
+            errorCallback.onError();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviews.size();
+    }
+
+}
---
+++ app/src/main/java/com/gabrielfeo/backintheday/ui/moviedetails/adapter/ReviewViewHolder.java
@@ -0,0 +1,24 @@
+package com.gabrielfeo.backintheday.ui.moviedetails.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.gabrielfeo.backintheday.R;
+
+final class ReviewViewHolder extends RecyclerView.ViewHolder {
+
+    final TextView authorView;
+    final TextView contentView;
+    final Button readMoreButton;
+
+    public ReviewViewHolder(@NonNull View itemView) {
+        super(itemView);
+        authorView = itemView.findViewById(R.id.moviedetailreviews_tv_review_author);
+        contentView = itemView.findViewById(R.id.moviedetailreviews_tv_review_content);
+        readMoreButton = itemView.findViewById(R.id.moviedetail_b_reviews_read_more_button);
+    }
+
+}
